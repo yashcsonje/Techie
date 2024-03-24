f24cbf005072978db9dc49029d490b218db3603a
yashcsonje <162028752+yashcsonje@users.noreply.github.com>
2024-03-25 01:26:23 +0530

    Add files via upload

---
+++ Dictionary/methods.py
@@ -0,0 +1,13 @@
+student = {'name' : 'Alice' , 'age' : 21 , 'grade' : 'A'} 
+# Get all keys
+keys = student.keys()
+keys
+dict_keys(['name', 'age', 'grade'])
+# Get all values
+values = student.values()
+values
+dict_values(['Alice', 21, 'A'])
+# Get items as key-value pair
+items = student.items()
+items
+dict_items([('name', 'Alice'), ('age', 21), ('grade', 'A')])
