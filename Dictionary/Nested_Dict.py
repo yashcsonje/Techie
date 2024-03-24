a0a01ce172fc515a5530dec17890378a16334333
yashcsonje <162028752+yashcsonje@users.noreply.github.com>
2024-03-25 01:36:35 +0530

    Add files via upload

---
+++ Dictionary/Nested_Dict.py
@@ -0,0 +1,6 @@
+contacts = {
+    'Alice' : {'phone' : 1234567890 , 'email' : 'alice@gmail.com'},
+    'Bob' : {'phone' : 6789012345 , 'email' : 'bob@gmail.com'}
+}
+contacts['Bob']
+{'phone': 6789012345, 'email': 'bob@gmail.com'}
