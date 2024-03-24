90d2b1b73aeb2402c3cc27d330e6b1ff10f886d4
yashcsonje <162028752+yashcsonje@users.noreply.github.com>
2024-03-25 01:55:53 +0530

    Add files via upload

---
+++ List/Creation_Accessing.py
@@ -0,0 +1,14 @@
+# Creating an empty list 
+my_list = []
+
+# Creating list with elements 
+numbers = [1,2,3,4,5]
+fruits = ['apple','banana','carrot']
+mixed_list = [1,'hello',3.14,True] 
+# Accessing elements by index
+numbers[0]
+1
+fruits[-1]
+'carrot'
+mixed_list[0:2]
+[1, 'hello']
