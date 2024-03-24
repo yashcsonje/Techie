521b97336d5b712ed7075e3129c8371b5a9cadb0
yashcsonje <162028752+yashcsonje@users.noreply.github.com>
2024-03-24 23:43:47 +0530

    Add files via upload

---
+++ Dictionary/dic_create.py
@@ -0,0 +1,9 @@
+# Creating an empty Dictionary
+empty_dict = {}
+
+# Creating Dictionary with key-value pairs
+student = {'name' : 'Alice' , 'age' : 25 , 'grade' : 'A'}
+
+fruits = {1 : 'apple' , 2 : 'banana' , 3 : 'carrot'}
+
+mixed_dict = {'key1' : 1 , 'key2' : 'hello' , 'key3' : True}
